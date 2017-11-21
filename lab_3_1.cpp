ef45b54ee4265e46b2d04dad64af934a4277214d
Unknown <tidarat.123pla@gmail.com>
2017-11-21 17:36:29 +0700

    ทำข้อ1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+
+  cout << "Mr." << "Tidarat Keawted" << endl;
+  cout << "nickname" << "pla" << endl;
+  cout << "sex" << "female" << endl;
+  cout << "birthday" << "December 5, 1998" << endl;
+  cout << "Hobbies" << "read a book" << endl;
+  cout << "Status" << "single" << endl;
+
   return 0;
 }
